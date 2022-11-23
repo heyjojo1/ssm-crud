35a7dbceae1dc16ece173025a63de1718b3a5c4a
jojo <jojo@123.com>
2022-11-23 14:54:54 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -10,6 +10,6 @@ public class Main {
         System.out.println("Hello Git!1");
         System.out.println("Hello Git!2");
         System.out.println("Hello Git!3");
-
+        System.out.println("hot-fix test");
     }
 }
